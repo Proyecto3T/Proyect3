970e0a626aeb6818af72c422212d459f630e316e
diegomendezp <diegomen15@gmail.com>
2018-07-31 12:35:36 +0200

    Profile

--- server/models/Match.js
+++ server/models/Match.js
@@ -6,6 +6,8 @@ const matchSchema = new Schema({
   date: {type: Date, default: Date.now()},
   place: String,
   result: String,
+  winner: {type: Schema.Types.ObjectId, ref:"User"},
+  loser:{type: Schema.Types.ObjectId, ref:"User"},
   finish: {type: boolean, default:false}
 }, {
   timestamps: {
