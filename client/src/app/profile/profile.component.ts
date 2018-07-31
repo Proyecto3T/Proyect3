063881e8c28a1d85ce3083f0bad60492589fd35d
Enrique Pernia <Epernia96@gmail.com>
2018-07-31 15:26:40 +0200

    Radar chart

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  public chart = [];
+  constructor( ) { 
+   
   }
 
+  ngOnInit() {
+    var canvas = <HTMLCanvasElement> document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+    this.chart =new Chart(ctx, {
+    type: 'radar',
+    
+    data: {
+      labels: ["Drive", "Backhand", "Serve", "Volley", "Resistance"],
+      datasets: [
+        {
+         label: "Your Statistics",
+          data: [2,5,7,3,5],
+          borderColor: 'rgba(20, 29, 222, 1)',
+          backgroundColor: 'rgba(20, 29, 222, 0.2)',
+
+        },
+        {
+          label: "  Media",
+          data: [1,2,5,8,4],
+          borderColor: 'rgba(255, 99, 132, 0.2)',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+
+        },
+      ]
+    },
+  options :{
+    scale: {
+        // Hides the scale
+        ticks: {
+          // changes here
+          max:10,
+          min:0
+      },
+     
+      
+    }
+},
+
+  })
+  }
+  
+ 
+                
 }
--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
@@ -9,5 +10,5 @@ import { AppComponent } from './app.component';
 export const routes: Routes = [
   { path:'signup', component:SignupFormComponent},
   { path:'login', component:LoginFormComponent},
-
+  {path:'profile', component:ProfileComponent}
 ];
